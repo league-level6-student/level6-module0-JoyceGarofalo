b2e2b9a9e027d03bad12530eec8feba812c00756
joycegarofalo <joycegarofalo@joyces-macbook.lan>
2021-09-19 11:26:26 -0700

    payroll test, newsapi test, intro to mocking ip

--- src/test/java/_06_payroll/PayrollTest.java
+++ src/test/java/_06_payroll/PayrollTest.java
@@ -11,28 +11,43 @@ class PayrollTest {
     @Test
     void itShouldCalculatePaycheck() {
         //given
-
+    	double hourlyWage = 4.2;
+    	int numHours = 5;
+    	double expected = 21;
+    	
         //when
-
+    	double actual = Payroll.calculatePaycheck(hourlyWage, numHours);
+    	
         //then
+    	assertEquals(expected, actual);
+    	
     }
 
     @Test
     void itShouldCalculateMileageReimbursement() {
         //given
-
+    	int milesTraveled = 12;
+    	double expected = 6.9;
+    	
         //when
+    	double actual = Payroll.calculateMileageReimbursement(milesTraveled);
 
         //then
+    	assertEquals(expected, actual);
     }
 
     @Test
     void itShouldCreateOfferLetter() {
         //given
+    	String employeeName = "Joyce";
+    	double hourlyWage = 13.5;
+    	String expected = "Hello Joyce, We are pleased to offer you an hourly wage of 13.5";
 
         //when
+    	String actual = Payroll.createOfferLetter(employeeName, hourlyWage);
 
         //then
+    	assertEquals(expected, actual);
     }
 
 }
